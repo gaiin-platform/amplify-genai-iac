8efacca4cef2be9fca7e559825f2fc59ae98828e
Allen Karns <allen.karns@vanderbilt.edu>
2024-11-12 12:26:04 -0600

    add vbc flow logs

--- modules/load_balancer/load_balancer.tf
+++ modules/load_balancer/load_balancer.tf
@@ -96,6 +96,59 @@ resource "aws_vpc" "main" {
   }
 }
 
+# Add these resources after the VPC resource
+
+resource "aws_flow_log" "vpc_flow_log" {
+  iam_role_arn    = aws_iam_role.vpc_flow_log_role.arn
+  log_destination = aws_cloudwatch_log_group.vpc_flow_log_group.arn
+  traffic_type    = "ALL"
+  vpc_id          = aws_vpc.main.id
+}
+
+resource "aws_cloudwatch_log_group" "vpc_flow_log_group" {
+  name = "/aws/vpc-flow-log/${aws_vpc.main.id}"
+  retention_in_days = 30
+}
+
+resource "aws_iam_role" "vpc_flow_log_role" {
+  name = "vpc-flow-log-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "vpc-flow-logs.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "vpc_flow_log_policy" {
+  name = "vpc-flow-log-policy"
+  role = aws_iam_role.vpc_flow_log_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:DescribeLogGroups",
+          "logs:DescribeLogStreams"
+        ]
+        Effect = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
 # Create public subnets in two different AZs
 resource "aws_subnet" "public" {
   count                   = length(var.public_subnet_cidrs)
