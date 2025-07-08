e12bb96e43959af0668be73f95de44b03807dcd4
Oscar Gonzalez <oscar.gonzalez@fortyau.com>
2025-07-07 22:32:55 -0300

    bump AWS provider to support aws_bedrock_guardrail

--- prod/provider.tf
+++ prod/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.15.0"
+      version = "~> 5.63.0"
     }
   }
   required_version = ">= 0.14.0"
